a7945505dea1c7f90e10247d455d12661b3cfb16
geetasri p <geeta.peddireddi@gmail.com>
2022-01-26 20:48:11 -0800

    To-Do-redux

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import axios from "axios";
+import { connect } from "react-redux";
 
 import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { RegistrationView } from "../registration-view/registration-view";
+import { setMovies } from "../../actions/actions";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -20,12 +22,12 @@ class MainView extends React.Component {
   }
 
   componentDidMount() {
+    console.log("component did mount");
     axios
       .get("https://my-movies-app-new.herokuapp.com/movies")
       .then(response => {
-        this.setState({
-          movies: response.data
-        });
+        console.log(response.data);
+        this.props.setMovies(response.data);
       })
       .catch(error => {
         console.log(error);
@@ -52,9 +54,9 @@ class MainView extends React.Component {
   }
 
   render() {
-    const { movies, selectedMovie, user, registered } = this.state;
-    console.log("user", user);
-    console.log("registerd", registered);
+    let { movies } = this.props;
+    let { user } = this.state;
+    console.log("movies", movies);
     // if (!registered)
     //   return (
     //     <RegistrationView
@@ -66,43 +68,45 @@ class MainView extends React.Component {
 
     if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
-    if (selectedMovie) return <MovieView movie={selectedMovie} />;
+    //if (selectedMovie) return <MovieView movie={selectedMovie} />;
 
     if (movies.length === 0) {
       return <div className="main-view">The list is empty!</div>;
     } else {
       return (
-        <div className="main-view">
-          {selectedMovie ? (
-            <Row className="justify-content-md-center">
-              <Col md={8}>
-                <MovieView
-                  movie={selectedMovie}
-                  onBackClick={newSelectedMovie => {
-                    this.setSelectedMovie(newSelectedMovie);
-                  }}
-                />
-              </Col>
-            </Row>
-          ) : (
-            <Row className="justify-content-md-center">
-              {movies.map(movie => (
-                <Col md={3}>
-                  <MovieCard
-                    key={movie._id}
-                    movie={movie}
-                    onMovieClick={movie => {
-                      this.setSelectedMovie(movie);
-                    }}
-                  />
-                </Col>
-              ))}
-            </Row>
-          )}
-        </div>
+        <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+
+        // <Router>
+        //   <Row className="main-view justify-content-md-center">
+        //     <Route
+        //       exact
+        //       path="/"
+        //       render={() => {
+        //         if (!user)
+        //           return (
+        //             <Col>
+        //               <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+        //             </Col>
+        //           );
+        //         if (movies.length === 0) return <div className="main-view" />;
+        //         // #6
+        //         return <MoviesList movies={movies} />;
+        //       }}
+        //     />
+        //     {/* The rest of routes */}
+        //   </Row>
+        // </Router>
       );
     }
   }
 }
 
-export default MainView;
+let mapStateToProps = state => {
+  return { movies: state.movies };
+};
+
+// #8
+export default connect(
+  mapStateToProps,
+  { setMovies }
+)(MainView);
