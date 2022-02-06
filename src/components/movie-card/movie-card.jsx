fe4488dcee701f48a97d7b8c92f9685b983121b1
geetasri p <geeta.peddireddi@gmail.com>
2022-02-06 13:45:19 -0800

    changing export type

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import { Card, CardColumns } from "react-bootstrap";
-import img from "../../img/images.jpeg";
+import { Link } from "react-router-dom";
+import movieimg from "../../img/images.jpeg";
 
-export class MovieCard extends React.Component {
+class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
-
+    const { movie } = this.props;
     return (
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={img} />
+        <Card.Img className="card-img" variant="top" src={movieimg} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
+          <Link className="link-secondary" to={`/movies/${movie._id}`}>
+            <Button variant="link" className="link-danger">
+              Open
+            </Button>
+          </Link>
         </Card.Body>
       </Card>
     );
@@ -23,3 +28,5 @@ MovieCard.prototype = {
   Title: PropTypes.string.isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
+
+export default MovieCard;
