157cdd13731c420a1b484f8ea0ae2ec18025631d
geetasri p <geeta.peddireddi@gmail.com>
2022-05-02 00:58:47 -0700

    Added validation to Login Screen

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,24 +9,56 @@ export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+  const [loginErr, setLoginErr] = useState("");
+
+  const validate = () => {
+    // Reset errors when validate runs
+    setUsernameErr("");
+    setPasswordErr("");
+
+    let isReq = true;
+    if (!username) {
+      setUsernameErr("Username Required");
+      isReq = false;
+    } else if (username.length < 2) {
+      setUsernameErr("Username must be at least 2 characters long");
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr("Password Required");
+      isReq = false;
+    } else if (password.length < 6) {
+      setPasswordErr("Password must be at least 6 characters");
+      isReq = false;
+    }
+
+    return isReq;
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
     console.log(username, password);
-    /* Send a request to the server for authentication */
-    axios
-      .post("https://my-movies-app-new.herokuapp.com/login", {
-        Username: username,
-        Password: password
-      })
-      .then(response => {
-        const data = response.data;
-        props.onLoggedIn(data);
-      })
-      .catch(e => {
-        console.log("User not found");
-      });
-    /* then call props.onLoggedIn(username) */
-    //props.onLoggedIn(username);
+    const isReq = validate();
+    if (isReq) {
+      /* Send a request to the server for authentication */
+      axios
+        .post("https://my-movies-app-new.herokuapp.com/login", {
+          Username: username,
+          Password: password
+        })
+        .then(response => {
+          const data = response.data;
+          props.onLoggedIn(data);
+        })
+        .catch(e => {
+          setLoginErr("Username or Password is incorrect");
+          console.log("User not found");
+        });
+      /* then call props.onLoggedIn(username) */
+      //props.onLoggedIn(username);
+    }
   };
 
   return (
@@ -40,6 +72,11 @@ export function LoginView(props) {
             value={username}
             onChange={e => setUsername(e.target.value)}
           />
+          {usernameErr ? (
+            <span className="login-error">{usernameErr}</span>
+          ) : (
+            ""
+          )}
         </Form.Group>
         <Form.Group controlId="formPassword">
           <Form.Label>Password:</Form.Label>
@@ -48,6 +85,12 @@ export function LoginView(props) {
             value={password}
             onChange={e => setPassword(e.target.value)}
           />
+          {passwordErr ? (
+            <span className="login-error">{passwordErr}</span>
+          ) : (
+            ""
+          )}
+          {loginErr ? <span className="login-error">{loginErr}</span> : ""}
         </Form.Group>
         <Form.Group>
           <Button
